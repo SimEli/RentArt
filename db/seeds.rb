52b832bb497f3c667a1d490bb4b0c69fb379c949
Simon Elicaste <simonelicaste@gmail.com>
2020-11-10 15:57:14 +0100

    seeding some test stuffs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# artworks = Artwork.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+
+puts "Creating artworks..."
+madonne = { name: "Madonne", description: "the madonne, virgin with her child", category: "painting" }
+the_thinker =  { name: "The Thinker of Rodin", description: "Beautiful masterpiece", category: "statue" }
+
+[ madonne, the_thinker ].each do |attributes|
+  artwork = Artwork.create!(attributes)
+  puts "Created #{artwork.name}"
+end
+puts "Finished!"
