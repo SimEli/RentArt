c1bc00a234025a394cced6fb950171054dc4f8e7
Nina Tomic <ninatomic1703@gmail.com>
2020-11-10 14:27:13 +0000

    Artworks controller, routes, views

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,2 +1,37 @@
 class ArtworksController < ApplicationController
+
+  def index
+    @artworks = Artwork.all
+  end
+
+  def show
+     @artwork = Artwork.find(params[:id])
+      # @artwork = Artwork.new
+  end
+
+  def new
+    @artwork = Artwork.new
+  end
+
+  def create
+    @artwork = Artwork.create(artwork_params)
+
+    if @artwork.save
+      redirect_to artwork_path(@artwork)
+    else
+      render "artworks/new"
+    end
+
+  end
+
 end
+
+
+  private
+
+   def artwork_params
+      params.require(:artwork).permit(:name, :description)
+    end
+
+
+
