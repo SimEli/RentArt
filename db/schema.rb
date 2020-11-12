d3a462760326606a3879cde9d1faa9e26c0a1dbf
Pauline Noel <paulinenoel@icloud.com>
2020-11-12 11:43:47 +0100

    Add dotenv - Protect my secret data in .env file and columns to artworks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_160916) do
+ActiveRecord::Schema.define(version: 2020_11_12_101917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2020_11_09_160916) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "artist"
+    t.string "price"
+    t.integer "date"
+    t.string "size"
+    t.string "city"
   end
 
   create_table "bookings", force: :cascade do |t|
